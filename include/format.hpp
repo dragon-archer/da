1f2fccf299dc3a53ab18256c28ab6ac8ffb8b1df
dragon-archer <dragon-archer@outlook.com>
2022-07-17 17:44:15 +0800

    Add support for <format> in C++20, and its fallback solution fmtlib(currently version 9.0.0)

---
+++ include/format.hpp
@@ -0,0 +1,32 @@
+/**
+ * @file      format.hpp
+ * @brief     A wrapper for std::format or fmt::format if std::format doesn't exist
+ * @version   0.1
+ * @author    dragon-archer (dragon-archer@outlook.com)
+ *
+ * @copyright Copyright (c) 2022
+ */
+
+#ifndef _UTILITY_FORMAT_HPP_
+#define _UTILITY_FORMAT_HPP_
+
+#include "config.hpp"
+
+#if UTILITY_HAS_INCLUDE(<format>) // If there is format library as C++ standard, use it
+#include <format>
+namespace da {
+	using format = std::format;
+}
+#elif UTILITY_HAS_INCLUDE(<fmt/format.h>) // Otherwise if there is fmt library already installed in the system, use it
+#include <fmt/format.h>
+namespace da {
+	namespace format = ::fmt;
+}
+#elif UTILITY_HAS_INCLUDE("../thirdparty/fmt/include/fmt/format.h") // Finally, use our version of fmt. Current version: 9.0.0
+#include "../thirdparty/fmt/include/fmt/format.h"
+namespace da {
+	namespace format = ::fmt;
+}
+#endif
+
+#endif // _UTILITY_FORMAT_HPP_

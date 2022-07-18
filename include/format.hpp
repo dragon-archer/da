939e6db04ce21f4a8bfcf8d93de275ac0b03bba4
dragon-archer <dragon-archer@outlook.com>
2022-07-18 10:56:04 +0800

    Use libfmt header only

--- include/format.hpp
+++ include/format.hpp
@@ -18,11 +18,13 @@ namespace da {
 	using format = std::format;
 }
 #elif UTILITY_HAS_INCLUDE(<fmt/format.h>) // Otherwise if there is fmt library already installed in the system, use it
+#define FMT_HEADER_ONLY
 #include <fmt/format.h>
 namespace da {
 	namespace format = ::fmt;
 }
 #elif UTILITY_HAS_INCLUDE("../thirdparty/fmt/include/fmt/format.h") // Finally, use our version of fmt. Current version: 9.0.0
+#define FMT_HEADER_ONLY
 #include "../thirdparty/fmt/include/fmt/format.h"
 namespace da {
 	namespace format = ::fmt;

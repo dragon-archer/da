e81f73f8718b17608ad4e816247e5e01e2bd394c
dragon-archer <dragon-archer@outlook.com>
2022-11-12 11:08:44 +0800

    Remove boundled libfmt

--- include/format.hpp
+++ include/format.hpp
@@ -14,21 +14,22 @@
 
 #if DA_HAS_INCLUDE(<format>) // If there is format library as C++ standard, use it
 #include <format>
-namespace da {
-	using format = std::format;
+DA_BEGIN_NAMESPACE
+namespace fmt {
+	using std::format;
+	using std::format_to;
+	using std::format_to_n;
+	using std::formatted_size;
+	using std::vformat;
+	using std::vformat_to;
 }
-#elif DA_HAS_INCLUDE(<fmt/format.h>) // Otherwise if there is fmt library already installed in the system, use it
+DA_END_NAMESPACE
+#elif DA_HAS_INCLUDE(<fmt / format.h>) // Otherwise if there is fmt library already installed in the system, use it
 #define FMT_HEADER_ONLY
 #include <fmt/format.h>
-namespace da {
-	namespace format = ::fmt;
-}
-#elif DA_HAS_INCLUDE("../thirdparty/fmt/include/fmt/format.h") // Finally, use our version of fmt. Current version: 9.0.0
-#define FMT_HEADER_ONLY
-#include "../thirdparty/fmt/include/fmt/format.h"
-namespace da {
-	namespace format = ::fmt;
-}
+DA_BEGIN_NAMESPACE
+namespace fmt = ::fmt;
+DA_END_NAMESPACE
 #endif
 
 #endif // _LIBDA_FORMAT_HPP_

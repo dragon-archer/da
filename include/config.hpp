e489764af25e935182d301baf76e99ee643c4485
dragon-archer <dragon-archer@outlook.com>
2023-01-10 08:19:45 +0800

    Fix config.hpp

--- include/config.hpp
+++ include/config.hpp
@@ -2,9 +2,9 @@
  * @file      config.hpp
  * @brief     Some basic configs
  * @version   0.1
- * @author    dragon-archer (dragon-archer@outlook.com)
+ * @author    dragon-archer
  *
- * @copyright Copyright (c) 2022
+ * @copyright Copyright (c) 2022- 2023
  */
 
 #ifndef _LIBDA_CONFIG_HPP_
@@ -18,10 +18,8 @@
 /// Editable configs
 /// Uncomment following lines to enable
 
-// #define DA_NO_EXCEPTIONS // Disable exceptions
+// #define DA_NO_EXCEPTION // Disable exception
 // #define DA_NO_NAMESPACE // Disable namespace
-
-// clang-format off
 
 /// Verify we have at least C++20
 #if !defined(__cplusplus) || __cplusplus < 202002L
@@ -29,6 +27,12 @@
 #endif
 
 /// Feature test macros
+#if defined(__has_builtin)
+	#define DA_HAS_BUILTIN(x) __has_builtin(x)
+#else
+	#define DA_HAS_BUILTIN(x) 0
+#endif
+
 #if defined(__has_cpp_attribute)
 	#define DA_HAS_CPP_ATTRIBUTE(x) __has_cpp_attribute(x)
 #else
@@ -43,48 +47,39 @@
 
 /// Likely & unlikely
 #if DA_HAS_CPP_ATTRIBUTE(likely)
-	#define DA_IFLIKELY(x)	 if(x) [[likely]]
+	#define DA_IFLIKELY(x)   if(x) [[likely]]
 	#define DA_IFUNLIKELY(x) if(x) [[unlikely]]
-#elif DA_11 // Most compiler support likely & unlikely since C++11
-	#define DA_IFLIKELY(x)	 if(x) [[likely]]
-	#define DA_IFUNLIKELY(x) if(x) [[unlikely]]
+#elif DA_HAS_BUILTIN(__builtin_expect)
+	#define DA_IFLIKELY(x)   if(__builtin_expect(!!(x), 1))
+	#define DA_IFUNLIKELY(x) if(__builtin_expect(!!(x), 0))
 #else
-	#define DA_IFLIKELY(x)	 if(x)
+	#define DA_IFLIKELY(x)   if(x)
 	#define DA_IFUNLIKELY(x) if(x)
 #endif
 
 /// Exception
 #ifndef DA_NO_EXCEPTION
 	#include <stdexcept>
-	#define DA_TRY		   try
-	#define DA_CATCH(x)	   catch(x)
-	#define DA_THROW(x)	   throw(x)
-	#define DA_THROW_AGAIN throw
+	#define DA_TRY           try
+	#define DA_CATCH(x)      catch(x)
+	#define DA_THROW(x)      throw(x)
+	#define DA_THROW_AGAIN() throw
 #else
-	#define DA_TRY		   DA_IFLIKELY(true)
-	#define DA_CATCH	   DA_IFUNLIKELY(false)
-	#define DA_THROW(x)	   std::terminate()
-	#define DA_THROW_AGAIN std::terminate()
+	#define DA_TRY           DA_IFLIKELY(true)
+	#define DA_CATCH(x)      DA_IFUNLIKELY(false)
+	#define DA_THROW(x)      std::terminate()
+	#define DA_THROW_AGAIN() std::terminate()
 #endif
 
 /// Namespace
 #ifndef DA_NO_NAMESPACE
 	#define DA_BEGIN_NAMESPACE namespace da {
-	#define DA_END_NAMESPACE }
-	#define DA_NAMESAPCE ::da::
+	#define DA_END_NAMESPACE   }
+	#define DA_NAMESAPCE       ::da
 #else
 	#define DA_BEGIN_NAMESPACE
 	#define DA_END_NAMESPACE
 	#define DA_NAMESPACE
 #endif
-
-// clang-format on
-
-DA_BEGIN_NAMESPACE
-
-using std::ptrdiff_t;
-using std::size_t;
-
-DA_END_NAMESPACE
 
 #endif // _LIBDA_CONFIG_HPP_

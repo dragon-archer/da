39bca81265178d31afa93a1d172c18d279915430
dragon-archer <dragon-archer@outlook.com>
2023-02-10 22:28:48 +0800

    [preprocessor] Fix DA_IS_EMPTY * Fix a typo in DA_IS_EMPTY_I * Remove unnecessary DA_EXPAND in DA_IS_EMPTY, cause it may result to reentrence of DA_EXPAND, which then cause an error

--- include/preprocessor/empty.hpp
+++ include/preprocessor/empty.hpp
@@ -16,7 +16,7 @@
 /**
  * @brief Detect whether the argument is empty
  */
-#define DA_IS_EMPTY(...)   DA_IS_EMPTY_I(DA_EXPAND(__VA_ARGS__))
-#define DA_IS_EMTPY_I(...) DA_SEQ_HEAD(__VA_OPT__(0, ) 1)
+#define DA_IS_EMPTY(...)   DA_IS_EMPTY_I(__VA_ARGS__)
+#define DA_IS_EMPTY_I(...) DA_SEQ_HEAD(__VA_OPT__(0, ) 1)
 
 #endif // _LIBDA_PREPROCESSOR_EMPTY_HPP_

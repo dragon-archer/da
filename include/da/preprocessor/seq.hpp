6a3638e8201b065a843d9135593f3db0e191a7a0
dragon-archer <dragon-archer@outlook.com>
2023-02-13 23:22:16 +0800

    [preprocessor] feat: Extend sequence and tuple operations * Add macros to (get | remove | insert) (head | tail) of a (sequence | tuple)

--- include/da/preprocessor/seq.hpp
+++ include/da/preprocessor/seq.hpp
@@ -3,12 +3,14 @@
  * @brief     Macros for sequence operations
  * @version   0.1
  * @author    dragon-archer
- * @note      In this header, the term "sequence" means:
- *            1. Empty, contains nothing
- *            2. A comma separated list, while the length is not limited
- * @note      Though there is no hard limits of the sequence length,
- *            it's recommended not to exceed DA_EXPAND_MAX (currently 256),
- *            or some of the macros might fail.
+ *
+ * The term "sequence" means:
+ * 1. Empty, contains nothing
+ * 2. A comma separated list, while the length is not limited
+ *
+ * Though there is no hard limits of the sequence length,
+ * it's recommended not to exceed DA_EXPAND_MAX (currently 256),
+ * or some of the macros might fail.
  *
  * @copyright Copyright (c) 2023
  */
@@ -28,6 +30,11 @@
 #define DA_SEQ_HEAD_I(head, ...) head
 
 /**
+ * @brief Returns the last element of a sequence
+ */
+#define DA_SEQ_TAIL(...) DA_SEQ_HEAD(DA_SEQ_REVERSE(__VA_ARGS__))
+
+/**
  * @brief Reverse a sequence
  */
 #define DA_SEQ_REVERSE(...) \
@@ -38,11 +45,6 @@
 #define DA_SEQ_REVERSE_R() DA_SEQ_REVERSE_I
 
 /**
- * @brief Returns the last element of a sequence
- */
-#define DA_SEQ_TAIL(...) DA_SEQ_HEAD(DA_SEQ_REVERSE(__VA_ARGS__))
-
-/**
  * @brief Get nth param of a sequence
  */
 #define DA_SEQ_GET(n, ...) DA_EXPAND(DA_SEQ_GET_I(n, __VA_ARGS__))
@@ -51,5 +53,16 @@
 	DA_PARENS()                      \
 	DA_IF(n, (DA_DEC(n) __VA_OPT__(, ) __VA_ARGS__), _1)
 #define DA_SEQ_GET_R() DA_SEQ_GET_I
+
+/**
+ * @brief Remove the first element of a sequence
+ */
+#define DA_SEQ_REMOVE_HEAD(head, ...) __VA_ARGS__
+
+/**
+ * @brief Remove the last element of a sequence
+ */
+#define DA_SEQ_REMOVE_TAIL(...)   DA_SEQ_REVERSE(DA_SEQ_REMOVE_TAIL_I(DA_SEQ_REVERSE(__VA_ARGS__)))
+#define DA_SEQ_REMOVE_TAIL_I(...) DA_SEQ_REMOVE_HEAD(__VA_ARGS__)
 
 #endif // _DA_PREPROCESSOR_SEQ_HPP_
--- include/da/preprocessor/tuple.hpp
+++ include/da/preprocessor/tuple.hpp
@@ -34,11 +34,45 @@
 /**
  * @brief Get the size of a tuple
  */
-#define DA_TUPLE_SIZE(x) DA_CNT(DA_TUPLE_UNPACK(x))
+#define DA_TUPLE_SIZE(x) DA_CNT x
 
 /**
  * @brief Get nth element of a tuple
  */
 #define DA_TUPLE_GET(n, x) DA_SEQ_GET(n, DA_TUPLE_UNPACK(x))
+
+/**
+ * @brief Get the first element of a tuple
+ */
+#define DA_TUPLE_HEAD(x) DA_SEQ_HEAD x
+/**
+ * @brief Get the last element of a tuple
+ */
+#define DA_TUPLE_TAIL(x) DA_SEQ_TAIL x
+
+/**
+ * @brief Reverse a tuple
+ */
+#define DA_TUPLE_REVERSE(x) (DA_SEQ_REVERSE x)
+
+/**
+ * @brief Remove the first element of a tuple
+ */
+#define DA_TUPLE_REMOVE_HEAD(x) (DA_SEQ_REMOVE_HEAD x)
+
+/**
+ * @brief Remove the last element of a tuple
+ */
+#define DA_TUPLE_REMOVE_TAIL(x) (DA_SEQ_REMOVE_TAIL x)
+
+/**
+ * @brief Insert at the tail of a tuple
+ */
+#define DA_TUPLE_INSERT_TAIL(x, ...) (DA_TUPLE_UNPACK(x) __VA_OPT__(, ) __VA_ARGS__)
+
+/**
+ * @brief Insert at the front of a tuple
+ */
+#define DA_TUPLE_INSERT_HEAD(x, ...) (__VA_ARGS__ __VA_OPT__(, ) DA_TUPLE_UNPACK(x))
 
 #endif // _DA_PREPROCESSOR_TUPLE_HPP_

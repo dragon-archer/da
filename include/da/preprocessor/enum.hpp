0896ab08b8957c3405cc07fe05618412482cc160
dragon-archer <dragon-archer@outlook.com>
2023-02-14 08:52:52 +0800

    [preprocessor] feat: Extend enum support * Add macros to automatically define a mask enum

--- include/da/preprocessor/enum.hpp
+++ include/da/preprocessor/enum.hpp
@@ -11,57 +11,80 @@
 #define _DA_PREPROCESSOR_ENUM_HPP_
 
 #include <da/preprocessor/base.hpp>
+#include <da/preprocessor/tuple.hpp>
+#include <da/preprocessor/while.hpp>
 #include <type_traits>
 
 /**
- * @section DA_ASSERT_ENUM
- * @brief   Assert @param enum_ is an enum
+ * @brief Define a mask enum e
  */
-#define DA_ASSERT_ENUM(enum_) \
-	static_assert(std::is_enum_v<enum_>, DA_STR(enum_) " is not an enum")
+#define DA_DEFINE_MASK_ENUM(e, ...)                \
+	enum e {                                       \
+		DA_TUPLE_UNPACK(                           \
+			DA_TUPLE_REMOVE_HEAD(                  \
+				DA_TUPLE_GET(                      \
+					2,                             \
+					DA_WHILE(                      \
+						DA_DEFINE_MASK_ENUM_P,     \
+						DA_DEFINE_MASK_ENUM_O,     \
+						(0, (__VA_ARGS__), ()))))) \
+	};
+#define DA_DEFINE_MASK_ENUM_P(d) DA_TUPLE_SIZE(DA_TUPLE_GET(1, d))
+#define DA_DEFINE_MASK_ENUM_O(d)               \
+	(DA_INC(DA_TUPLE_HEAD(d)),                 \
+	 DA_TUPLE_REMOVE_HEAD(DA_TUPLE_GET(1, d)), \
+	 DA_TUPLE_INSERT_TAIL(                     \
+		 DA_TUPLE_GET(2, d),                   \
+		 DA_TUPLE_HEAD(DA_TUPLE_GET(1, d)) = 1 << DA_TUPLE_HEAD(d)))
 
 /**
- * @section DA_ENUM_BINARY_OP
- * @brief   Define binary operation @param op for @param enum_
+ * @brief Assert e is an enum
  */
-#define DA_ENUM_BINARY_OP(enum_, op)                                       \
-	DA_ASSERT_ENUM(enum_);                                                 \
-	inline constexpr enum_ operator op(enum_ x, enum_ y) {                 \
-		using t = std::underlying_type_t<enum_>;                           \
-		return static_cast<enum_>(static_cast<t>(x) op static_cast<t>(y)); \
-	}                                                                      \
+#define DA_ASSERT_ENUM(e) \
+	static_assert(std::is_enum_v<e>, DA_STR(e) " is not an enum")
+
+/**
+ * @brief Define binary operation op for e
+ */
+#define DA_ENUM_BINARY_OP(e, op)                                       \
+	DA_ASSERT_ENUM(e);                                                 \
+	inline constexpr e operator op(e x, e y) {                         \
+		using t = std::underlying_type_t<e>;                           \
+		return static_cast<e>(static_cast<t>(x) op static_cast<t>(y)); \
+	}                                                                  \
 	DA_STATEMENT()
 
 /**
- * @section DA_ENUM_UNARY_OP
- * @brief   Define unary operation @param op for @param enum_
+ * @brief Define unary operation op for e
  */
-#define DA_ENUM_UNARY_OP(enum_, op)                      \
-	DA_ASSERT_ENUM(enum_);                               \
-	inline constexpr enum_ operator op(enum_ x) {        \
-		using t = std::underlying_type_t<enum_>;         \
-		return static_cast<enum_>(op static_cast<t>(x)); \
-	}                                                    \
+#define DA_ENUM_UNARY_OP(e, op)                      \
+	DA_ASSERT_ENUM(e);                               \
+	inline constexpr e operator op(e x) {            \
+		using t = std::underlying_type_t<e>;         \
+		return static_cast<e>(op static_cast<t>(x)); \
+	}                                                \
 	DA_STATEMENT()
 
 /**
- * @section DA_ENUM_ASSIGN_OP
- * @brief   Define assignment operation @param op= for @param enum_
+ * @brief Define assignment operation op= for e
  */
-#define DA_ENUM_ASSIGN_OP(enum_, op)                                     \
-	DA_ASSERT_ENUM(enum_);                                               \
-	inline constexpr enum_& operator DA_CAT2(op, =)(enum_& x, enum_ y) { \
-		return x = x op y;                                               \
-	}                                                                    \
+#define DA_ENUM_ASSIGN_OP(e, op)                             \
+	DA_ASSERT_ENUM(e);                                       \
+	inline constexpr e& operator DA_CAT2(op, =)(e& x, e y) { \
+		return x = x op y;                                   \
+	}                                                        \
 	DA_STATEMENT()
 
-#define DA_MASK_ENUM_OPS(enum_)  \
-	DA_ENUM_BINARY_OP(enum_, &); \
-	DA_ENUM_BINARY_OP(enum_, |); \
-	DA_ENUM_BINARY_OP(enum_, ^); \
-	DA_ENUM_ASSIGN_OP(enum_, &); \
-	DA_ENUM_ASSIGN_OP(enum_, |); \
-	DA_ENUM_ASSIGN_OP(enum_, ^); \
-	DA_ENUM_UNARY_OP(enum_, ~);
+/**
+ * @brief Define bit operations for enum e
+ */
+#define DA_DEFINE_ENUM_OPS(e) \
+	DA_ENUM_BINARY_OP(e, &);  \
+	DA_ENUM_BINARY_OP(e, |);  \
+	DA_ENUM_BINARY_OP(e, ^);  \
+	DA_ENUM_ASSIGN_OP(e, &);  \
+	DA_ENUM_ASSIGN_OP(e, |);  \
+	DA_ENUM_ASSIGN_OP(e, ^);  \
+	DA_ENUM_UNARY_OP(e, ~);
 
 #endif // _DA_PREPROCESSOR_ENUM_HPP_

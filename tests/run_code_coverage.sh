51afd0aea1beb6b4b7e576b963f968275bcf6470
dragon-archer <dragon-archer@outlook.com>
2023-02-25 18:07:47 +0800

    [test] feat: Add script to easily build code coverage

---
+++ tests/run_code_coverage.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+cd $(dirname $0) && \
+mkdir -p ../out/coverage && \
+cd ../out/coverage && 
+cmake -S../.. -B. -GNinja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS="--coverage -fprofile-arcs -ftest-coverage" -DDA_BuildTests=ON -DCMAKE_CXX_COMPILER=g++ && \
+cmake --build . && 
+lcov -d . -z && \
+ctest --output-on-failure && \
+lcov -d . -c -o cover.info --rc lcov_branch_coverage=1 && \
+lcov -e cover.info "*include/da/*" -o filtered.info --rc lcov_branch_coverage=1 && \
+genhtml -t DA --legend --demangle-cpp -o html -s --branch-coverage filtered.info

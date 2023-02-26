197546d8e199129dfa6be3269cce2b0d91a3942e
dragon-archer <dragon-archer@outlook.com>
2023-02-26 19:30:49 +0800

    [preprocessor] fix: Fix DA_DEFINE_[SCOPED_]MASK_ENUM * Use DA_FOLD_LEFT instead of DA_WHILE

--- include/da/preprocessor/enum.hpp
+++ include/da/preprocessor/enum.hpp
@@ -11,42 +11,35 @@
 #define _DA_PREPROCESSOR_ENUM_HPP_
 
 #include <da/preprocessor/base.hpp>
+#include <da/preprocessor/fold.hpp>
 #include <da/preprocessor/tuple.hpp>
-#include <da/preprocessor/while.hpp>
 #include <type_traits>
 
 /**
  * @brief Define a scoped mask enum e
  */
-#define DA_DEFINE_SCOPED_MASK_ENUM(e, ...) \
-	enum class e {                         \
-		DA_DEFINE_MASK_ENUM_I(__VA_ARGS__) \
+#define DA_DEFINE_SCOPED_MASK_ENUM(e, ...)             \
+	enum class e {                                     \
+		__VA_OPT__(DA_DEFINE_MASK_ENUM_I(__VA_ARGS__)) \
 	}
 
 /**
  * @brief Define a mask enum e
  */
-#define DA_DEFINE_MASK_ENUM(e, ...)        \
-	enum e {                               \
-		DA_DEFINE_MASK_ENUM_I(__VA_ARGS__) \
+#define DA_DEFINE_MASK_ENUM(e, ...)                    \
+	enum e {                                           \
+		__VA_OPT__(DA_DEFINE_MASK_ENUM_I(__VA_ARGS__)) \
 	}
 
-#define DA_DEFINE_MASK_ENUM_I(...)         \
-	DA_TUPLE_UNPACK(                       \
-		DA_TUPLE_REMOVE_HEAD(              \
-			DA_TUPLE_GET(                  \
-				2,                         \
-				DA_WHILE(                  \
-					DA_DEFINE_MASK_ENUM_P, \
-					DA_DEFINE_MASK_ENUM_O, \
-					(0, (__VA_ARGS__), ())))))
-#define DA_DEFINE_MASK_ENUM_P(d) DA_TUPLE_SIZE(DA_TUPLE_GET(1, d))
-#define DA_DEFINE_MASK_ENUM_O(d)               \
-	(DA_INC(DA_TUPLE_HEAD(d)),                 \
-	 DA_TUPLE_REMOVE_HEAD(DA_TUPLE_GET(1, d)), \
-	 DA_TUPLE_INSERT_TAIL(                     \
-		 DA_TUPLE_GET(2, d),                   \
-		 DA_TUPLE_HEAD(DA_TUPLE_GET(1, d)) = 1 << DA_TUPLE_HEAD(d)))
+// clang-format off
+#define DA_DEFINE_MASK_ENUM_I(x, ...) \
+	x = 1                             \
+	DA_TUPLE_UNPACK(                  \
+		DA_TUPLE_REMOVE_HEAD(         \
+			DA_FOLD_LEFT(DA_DEFINE_MASK_ENUM_O, (x), __VA_ARGS__)))
+// clang-format on
+#define DA_DEFINE_MASK_ENUM_O(x, y) \
+	(y, DA_TUPLE_UNPACK(DA_TUPLE_REMOVE_HEAD(x)), y = DA_TUPLE_HEAD(x) << 1)
 
 /**
  * @brief Assert e is an enum

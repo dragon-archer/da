7c8381d54472e1e9ed27380bcc8ef28733c3673c
dragon-archer <dragon-archer@outlook.com>
2023-02-26 13:57:37 +0800

    [cmake] fix: Fix code coverage

--- cmake/coverage.cmake
+++ cmake/coverage.cmake
@@ -10,7 +10,8 @@
 add_custom_target(code_coverage
 	COMMAND ${CMAKE_COMMAND} -S${PROJECT_SOURCE_DIR} -B${PROJECT_BINARY_DIR}/code_coverage
 		-DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS="--coverage;-fprofile-arcs;-ftest-coverage;-DDA_ON_CODE_COVERAGE" -DDA_BuildTests=ON -DCMAKE_CXX_COMPILER=g++
-	COMMAND ${CMAKE_COMMAND} --build ${PROJECT_BINARY_DIR}/
+	COMMAND ${CMAKE_COMMAND} --build ${PROJECT_BINARY_DIR}
+	COMMAND lcov -d . -z
 	COMMAND cd ${PROJECT_BINARY_DIR}/code_coverage && ${CMAKE_CTEST_COMMAND} --output-on-failure
 	COMMAND lcov -d . -c -o cover.info --rc lcov_branch_coverage=1
 	COMMAND lcov -e cover.info "*include/da/*" -o filtered.info --rc lcov_branch_coverage=1

0d428fefaaf51a4c0a326c14fc5143182efe5964
dragon-archer <dragon-archer@outlook.com>
2023-03-11 13:19:29 +0800

    [type_traits] feat: Create module type_traits * Add basic config

---
+++ include/da/type_traits/config.hpp
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: MIT */
+/**
+ * @file      config.hpp
+ * @brief     Config for type_traits
+ * @version   0.1
+ * @author    dragon-archer
+ *
+ * @copyright Copyright (c) 2023 dragon-archer
+ */
+
+#ifndef _DA_TYPE_TRAITS_CONFIG_HPP_
+#define _DA_TYPE_TRAITS_CONFIG_HPP_
+
+#include <da/config.hpp>
+#include <type_traits>
+
+#endif // _DA_TYPE_TRAITS_CONFIG_HPP_
